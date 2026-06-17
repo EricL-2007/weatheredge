02086eeb467f6991caa5449bbf88247bd86e0356
Eric Liu <ericliu@Erics-MacBook-Pro.local>
2026-06-17 12:43:21 -0500

    Update dashboard app.py

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -1,6 +1,8 @@
 import sys
 from pathlib import Path
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+
 ROOT_DIR = Path(__file__).resolve().parent
 if str(ROOT_DIR) not in sys.path:
     sys.path.append(str(ROOT_DIR))
